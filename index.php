6cae1a99bf4d547fd218fbaf240b1210aa9e3d7e
Muhamad Iqbal Amrullah <iqbalnfbsb@gmail.com>
2022-01-17 13:24:04 +0700

    update index.php
    add target="blank" for registration form

--- index.php
+++ index.php
@@ -279,7 +279,7 @@
               <a href="assets/guidelines/NurulFikriLeaderLeagueGuidelines.pdf" class="custom1-button active" download>Guidelines</a>
             </li>
             <li>
-              <a href="https://docs.google.com/forms/d/e/1FAIpQLSdJrW7ZlswDu2fJgpmyi2ic_oBSH0MgD6cmE9ljgprIvKqgQw/viewform" class="custom-button active">Register</a>
+              <a target="blank" href="https://docs.google.com/forms/d/e/1FAIpQLSdJrW7ZlswDu2fJgpmyi2ic_oBSH0MgD6cmE9ljgprIvKqgQw/viewform" class="custom-button active">Register</a>
             </li>
           </ul>
         </div>
@@ -341,7 +341,7 @@
                   <div class="games-content">
                     <h5 class="title"><a href="#">Science Project</a></h5>
                     <a href="assets/guidelines/ScienceProjectGuidelines.pdf" class="custom1-button active" download>Guidelines</a>
-                    <a href="https://docs.google.com/forms/d/e/1FAIpQLSf2PGq4bmg7YnmwiXzFZitg-0PISNi5xIp-EWZvqgOdHALKsw/viewform" class="custom-button active">Register</a>
+                    <a target="blank" href="https://docs.google.com/forms/d/e/1FAIpQLSf2PGq4bmg7YnmwiXzFZitg-0PISNi5xIp-EWZvqgOdHALKsw/viewform" class="custom-button active">Register</a>
                   </div>
                 </div>
               </div>
@@ -353,7 +353,7 @@
                   <div class="games-content">
                     <h5 class="title"><a href="#">LCC SAINTEK</a></h5>
                     <a href="assets/guidelines/lCC_IPA_Guidelines.pdf" class="custom1-button active" download>Guidelines</a>
-                    <a href="https://docs.google.com/forms/d/e/1FAIpQLSeesMGxkSMBCg9sUW0BGH9B5-8QkgnO113ebVpluPj6tNDu9Q/viewform" class="custom-button active">Register</a>
+                    <a target="blank" href="https://docs.google.com/forms/d/e/1FAIpQLSeesMGxkSMBCg9sUW0BGH9B5-8QkgnO113ebVpluPj6tNDu9Q/viewform" class="custom-button active">Register</a>
                   </div>
                 </div>
               </div>
@@ -506,7 +506,7 @@
                   <div class="games-content">
                     <h5 class="title"><a href="#">Speech</a></h5>
                     <a href="assets/guidelines/EnglishCompetitionsGuidelines.pdf" class="custom1-button active" download>Guidelines</a>
-                    <a href="https://docs.google.com/forms/d/e/1FAIpQLSeN5VOC-iKucnB2vuE9OMbwB7IBlmrw7NAwu95RqvHzbD6tFg/viewform" class="custom-button active">Register</a>
+                    <a target="blank" href="https://docs.google.com/forms/d/e/1FAIpQLSeN5VOC-iKucnB2vuE9OMbwB7IBlmrw7NAwu95RqvHzbD6tFg/viewform" class="custom-button active">Register</a>
                   </div>
                 </div>
               </div>
@@ -537,7 +537,7 @@
                   <div class="games-content">
                     <h5 class="title"><a href="#">Vlog</a></h5>
                     <a href="assets/guidelines/VlogDesainPostersPuisiGuidelines.pdf" class="custom1-button active" download>Guidelines</a>
-                    <a href="https://docs.google.com/forms/d/e/1FAIpQLSe6TNZmhhoH0_Ft2Vtq7qMlu6SmeTqrN7hqPv_hzgOpoG_amQ/viewform" class="custom-button active">Register</a>
+                    <a target="blank" href="https://docs.google.com/forms/d/e/1FAIpQLSe6TNZmhhoH0_Ft2Vtq7qMlu6SmeTqrN7hqPv_hzgOpoG_amQ/viewform" class="custom-button active">Register</a>
                   </div>
                 </div>
               </div>
@@ -556,7 +556,7 @@
                   <div class="games-content">
                     <h5 class="title"><a href="#">Poetry</a></h5>
                     <a href="assets/guidelines/VlogDesainPostersPuisiGuidelines.pdf" class="custom1-button active" download>Guidelines</a>
-                    <a href="https://docs.google.com/forms/d/e/1FAIpQLScDbmH2QhKkjHJp8yJH0qbhRc4Pgu_Ww9i3dyo9mR-TTTyWEQ/viewform" class="custom-button active">Register</a>
+                    <a target="blank" href="https://docs.google.com/forms/d/e/1FAIpQLScDbmH2QhKkjHJp8yJH0qbhRc4Pgu_Ww9i3dyo9mR-TTTyWEQ/viewform" class="custom-button active">Register</a>
                   </div>
                 </div>
               </div>
@@ -575,7 +575,7 @@
                   <div class="games-content">
                     <h5 class="title"><a href="#">Poster</a></h5>
                     <a href="assets/guidelines/VlogDesainPostersPuisiGuidelines.pdf" class="custom1-button active" download>Guidelines</a>
-                    <a href="https://docs.google.com/forms/d/e/1FAIpQLSdBHHkSQVYs9XmfkvfiYhejEkjkGR6JsX2UkyF3e8-cI7J1qQ/viewform" class="custom-button active">Register</a>
+                    <a target="blank" href="https://docs.google.com/forms/d/e/1FAIpQLSdBHHkSQVYs9XmfkvfiYhejEkjkGR6JsX2UkyF3e8-cI7J1qQ/viewform" class="custom-button active">Register</a>
                   </div>
                 </div>
               </div>
